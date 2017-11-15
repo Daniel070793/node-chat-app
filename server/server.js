2c6d98f4015a6b8d337ad14212c08b88d99c552e
Daniel Sørensen <1058493@ucn.dk>
2017-11-15 21:05:24 +0100

    add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected at: " + moment().format("LTS"));
 
+  socket.emit("newMessage", {
+    from: "someone",
+    text: "hej fra mig",
+    createdAt: 123123
+  });
+
+  socket.on("createMessage", message => {
+    console.log("createMessage", message);
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected at: " + moment().format("LTS"));
   });
