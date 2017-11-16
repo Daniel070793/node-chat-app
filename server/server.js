2df6132c3062c324b88e4ce21b7ec3bdd6438f66
Daniel Sørensen <1058493@ucn.dk>
2017-11-16 20:35:48 +0100

    created generateMessage function

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 const moment = require("moment");
+const { generateMessage } = require("./utils/message");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
@@ -15,25 +16,16 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected at: " + moment().format("LTS"));
 
-  socket.emit("newMessage", {
-    from: "Admin",
-    text: "Velkommen til chatten",
-    createdAt: new Date().getTime()
-  });
+  socket.emit("newMessage", generateMessage("Admin", "Velkommen til chatten."));
 
-  socket.broadcast.emit("newMessage", {
-    from: "Admin",
-    text: "Ny bruger tilsluttet",
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit(
+    "newMessage",
+    generateMessage("Admin", "Ny bruger tilsluttet")
+  );
 
   socket.on("createMessage", message => {
     console.log("createMessage", message);
-    io.emit("newMessage", {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit("newMessage", generateMessage(message.from, message.text));
 
     // socket.broadcast.emit("newMessage", {
     //   from: message.from,
